e5280ef16f4b4f83a54d7a4373220c016ecc688a
Aaron Blancaflor <ablancaflor@runding.com>
2023-08-22 20:43:05 -0700

    forgot to save

--- proj1-calculator/calculator.js
+++ proj1-calculator/calculator.js
@@ -28,8 +28,10 @@ const calculatorApp = async () => {
         const operator = await rl.question("Enter desired operation (+, -, /, *): ");
         const secondOperand = await rl.question("Enter second operand: ")
         const equation = firstOperand + operator + secondOperand;
+        
         let result = calculate(parseInt(firstOperand), operator, parseInt(secondOperand));
         runningTotal += result;
+        
         console.log(`equation is: ${equation}`);
         console.log(`result is: ${result}`);
         console.log(`running total: ${runningTotal}`);
