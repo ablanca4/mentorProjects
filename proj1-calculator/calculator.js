e3a15f87f4985899613043dd93d9aae54d860096
Aaron Blancaflor <ablancaflor@runding.com>
2023-08-24 11:35:18 -0700

    Refactor based on mento meeting

--- proj1-calculator/calculator.js
+++ proj1-calculator/calculator.js
@@ -1,11 +1,13 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import { error } from 'node:console';
 
 
+const rl = readline.createInterface(input, output);
 // given an equation will calculate the result
-const calculate = (firstOperand, optation, secondOperand) => {
+const calculate = (firstOperand, operation, secondOperand) => {
 
-    switch(optation) {
+    switch(operation) {
         
         case '+':
             return firstOperand + secondOperand;
@@ -15,21 +17,20 @@ const calculate = (firstOperand, optation, secondOperand) => {
             return firstOperand * secondOperand;
         case '/':
             return firstOperand / secondOperand;
+        default:
+            throw Error(`${operation} is not a valid arithmatic operation.`);
     }
-
-    return equationResult;
 }
 
 const calculatorApp = async () => {
     let runningTotal = 0;
     while(true){
-        const rl = readline.createInterface(input, output);
+        
         const firstOperand = await rl.question("Enter first operand: ");
         const operator = await rl.question("Enter desired operation (+, -, /, *): ");
         const secondOperand = await rl.question("Enter second operand: ")
         const equation = firstOperand + operator + secondOperand;
-        
-        let result = calculate(parseInt(firstOperand), operator, parseInt(secondOperand));
+        const result = calculate(parseInt(firstOperand), operator, parseInt(secondOperand));
         runningTotal += result;
         
         console.log(`equation is: ${equation}`);
